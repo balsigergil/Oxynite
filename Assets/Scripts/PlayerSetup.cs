800c6447a04ec6e76334d565f57343a0d522bb93
Gil Balsiger <balsigergil@users.noreply.github.com>
2018-03-15 09:56:34 +0100

    Networking setup

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+
+    [SerializeField] Behaviour[] componentsToDisable;
+
+    private Camera sceneCam;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour component in componentsToDisable)
+            {
+                component.enabled = false;
+            }
+        }
+        else
+        {
+            sceneCam = Camera.main;
+            if(sceneCam)
+                sceneCam.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCam)
+            sceneCam.gameObject.SetActive(true);
+    }
+}
